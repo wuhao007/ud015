6e8da91796145fbfb9321aa9e7c915cbd2e77636
Hao Wu <tohaowu@gmail.com>
2016-02-07 16:34:10 -0800

    learn nothing

---
+++ library.js
@@ -0,0 +1,13 @@
+var Car = function(loc){
+    obj.loc = loc
+};
+Car.prototype.move = function(){
+    this.loc++;
+};
+
+var Van = function(loc){
+    Car.call(this, loc);
+};
+Van.prototype = Object.create(Car.prototype);
+Van.prototype.constructor = Van;
+Van.prototype.grab = function() { /*...*/ };
---
+++ run.js
@@ -0,0 +1,8 @@
+var zen = new Car(3);
+zen.move();
+
+var amy = new Car(9);
+console.log(amy.loc);
+amy.move();
+amy.grab();
+console.log(amy.constructor);
